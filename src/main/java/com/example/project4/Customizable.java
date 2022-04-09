d453da9d3599dbc3dfab10377c0e48bdb9dfac73
Rockorory <electronicwiz01@gmail.com>
2022-04-09 00:51:02 -0400

    Initial commit

---
+++ src/main/java/com/example/project4/Customizable.java
@@ -0,0 +1,10 @@
+/**
+ * This interface allows for the customizability of add-ons
+ * @author Rory Xu, Hassan Alfareed
+ */
+package com.example.project4;
+
+public interface Customizable {
+	boolean add(Object obj);
+	boolean remove(Object obj);
+}
