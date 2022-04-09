4fdf43c40c721bbdf5b935258ae1509217f451e5
Rockorory <electronicwiz01@gmail.com>
2022-04-09 14:43:10 -0400

    completed storefront view and button functionality to display other scenes

--- src/main/java/com/example/project4/Main.java
+++ src/main/java/com/example/project4/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("storeView.fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 800, 800);
+		Scene scene = new Scene(fxmlLoader.load(), 600, 400);
 		stage.setTitle("Rutgers Cafe");
 		stage.setScene(scene);
 		stage.show();
--- src/main/java/com/example/project4/StoreOrderViewController.java
+++ src/main/java/com/example/project4/StoreOrderViewController.java
@@ -1,8 +1,74 @@
 package com.example.project4;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class StoreOrderViewController {
 
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+
+	public void donutButtonClick(ActionEvent actionEvent) throws IOException {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("donutView.fxml"));
+			root = loader.load();
+			scene = new Scene(root, 600, 400);
+			stage = new Stage();
+			stage.setTitle("Donuts");
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void coffeeButtonClick(ActionEvent actionEvent) throws IOException {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("coffeeView.fxml"));
+			root = loader.load();
+			scene = new Scene(root, 600, 400);
+			stage = new Stage();
+			stage.setTitle("Coffee");
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void ordersButtonClick(ActionEvent actionEvent) throws IOException {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("orderView.fxml"));
+			root = loader.load();
+			scene = new Scene(root, 600, 400);
+			stage = new Stage();
+			stage.setTitle("Your Order");
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void storeOrdersButtonClick(ActionEvent actionEvent) throws IOException {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("storeOrderView.fxml"));
+			root = loader.load();
+			scene = new Scene(root, 600, 400);
+			stage = new Stage();
+			stage.setTitle("Store Orders");
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
